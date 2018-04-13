7aee6af354402c03686ec6bbb2a9be74309b0d17
CoderSherlock <haopengzhan@gmail.com>
2018-04-13 15:13:05 -0400

    Add Rules

---
+++ core/rules.cpp
@@ -0,0 +1,41 @@
+//
+// Created by pengzhan on 4/13/18.
+//
+
+#include "rules.h"
+
+using namespace core;
+
+int rules::add_Rules(std::string path, int type) {
+    for (auto i : this->ruleList) {
+        if(i.first.compare(path)) {
+            if(i.second == type) {
+                return 1;
+            }
+            else {
+                // TODO: Add logger
+                return 0;
+            }
+        }
+    }
+    this->ruleList.push_back(std::make_pair(path, type));
+    return 1;
+}
+
+int rules::add_Watch(std::string path) {
+    return this->add_Rules(path, rule_type::WATCH);
+}
+
+int rules::add_Ignore(std::string path) {
+    return this->add_Rules(path, rule_type::IGNORE);
+}
+
+int rules::add_Ignore_Universal(std::string path) {
+    // Check conflicts with specific ignore files.
+    return this->add_Rules(path, rule_type::IGNORE);
+}
+
+bool rules::check_status(std::string path) {
+    for(auto i : this->ruleList){
+    }
+}
---
+++ core/rules.h
@@ -0,0 +1,53 @@
+//
+// Created by pengzhan on 4/13/18.
+//
+
+#include <vector>
+#include <string>
+#include <utility>
+
+#ifndef PBACK_RULES_H
+#define PBACK_RULES_H
+
+
+
+namespace core {
+
+    enum rule_type{
+        WATCH = 1,
+        IGNORE = 2,
+        CHECK = 3
+    };
+
+
+    class rules {
+    private:
+        std::vector <std::pair<std::string, int> > ruleList;
+
+    public:
+        rules() {
+//TODO: Check if configuration has been done(Env)
+            restore_from_Disk();
+        };
+
+        virtual ~rules() {
+            sync_to_Disk();
+        };
+
+        int add_Rules(std::string, int);
+
+        int add_Watch(std::string);
+
+        int add_Ignore(std::string);
+
+        int add_Ignore_Universal(std::string);
+
+        bool check_status(std::string);
+
+        void sync_to_Disk();
+
+        void restore_from_Disk();
+
+    };
+}
+#endif //PBACK_RULES_H
